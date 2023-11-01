4d7fe203265a55b4576ff60e1a12eb446168d0f0
Ankit Kumar <ankitkr8540@gmail.com>
2023-11-01 12:09:30 -0400

    pmn-frontend app first commit

---
+++ pmn-frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          welcome to pmn-frontend Hackcathon
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
